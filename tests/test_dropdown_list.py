9004cc6cdf5156fd63aa80709eee190009faa010
BelozerovaLisa <jori15011998@gmail.com>
2024-12-30 11:19:21 +0300

    коммит2

--- tests/test_dropdown_list.py
+++ tests/test_dropdown_list.py
@@ -1,81 +1,32 @@
 from argparse import Action
 from time import sleep
 import allure
+import pytest
+
 from locators import Locators
 from constants import Constants
 
 from pages.scooter_page import ScooterPage
 
+answer_dict = [
+    {'question': Locators.ONE_QUESTION, 'answer': Locators.ONE_ANSWER, 'answer_text': Constants.ANSWER_ONE},
+    {'question': Locators.TWO_QUESTION, 'answer': Locators.TWO_ANSWER, 'answer_text': Constants.ANSWER_TWO},
+    {'question': Locators.THREE_QUESTION, 'answer': Locators.THREE_ANSWER, 'answer_text': Constants.ANSWER_THREE},
+    {'question': Locators.FOUR_QUESTION, 'answer': Locators.FOUR_ANSWER, 'answer_text': Constants.ANSWER_FOUR},
+    {'question': Locators.FIVE_QUESTION, 'answer': Locators.FIVE_ANSWER, 'answer_text': Constants.ANSWER_FIVE},
+    {'question': Locators.SIX_QUESTION, 'answer': Locators.SIX_ANSWER, 'answer_text': Constants.ANSWER_SIX},
+    {'question': Locators.SEVEN_QUESTION, 'answer': Locators.SEVEN_ANSWER, 'answer_text': Constants.ANSWER_SEVEN},
+    {'question': Locators.EIGHT_QUESTION, 'answer': Locators.EIGHT_ANSWER, 'answer_text': Constants.ANSWER_EIGHT}
+]
+
 @allure.title('Проверка выпадающего списка') # декораторы
 class TestDropdownList:
-    @allure.title('Тест открытия выпадающего ответа при нажатии на первый вопрос')  # декораторы
-    def test_first_question(self, driver):
+    @allure.title('Тест открытия выпадающего ответа при нажатии на вопрос')
+    @pytest.mark.parametrize("data", answer_dict)# декораторы
+    def test_first_question(self, driver, data):
         scooter_page = ScooterPage(driver)
         scooter_page.go_to_site()
         scooter_page.close_cookie()
-        scooter_page.click_one_question(Locators.ONE_QUESTION)
-        element = scooter_page.element_text(Locators.ONE_ANSWER)
-        assert element == Constants.ANSWER_ONE
-
-    @allure.title('Тест открытия выпадающего ответа при нажатии на второй вопрос')  # декораторы
-    def test_second_question(self, driver):
-        scooter_page = ScooterPage(driver)
-        scooter_page.go_to_site()
-        scooter_page.close_cookie()
-        scooter_page.click_one_question(Locators.TWO_QUESTION)
-        element = scooter_page.element_text(Locators.TWO_ANSWER)
-        assert element == Constants.ANSWER_TWO
-
-    @allure.title('Тест открытия выпадающего ответа при нажатии на третий вопрос')  # декораторы
-    def test_third_question(self, driver):
-        scooter_page = ScooterPage(driver)
-        scooter_page.go_to_site()
-        scooter_page.close_cookie()
-        scooter_page.click_one_question(Locators.THREE_QUESTION)
-        element = scooter_page.element_text(Locators.THREE_ANSWER)
-        assert element == Constants.ANSWER_THREE
-
-    @allure.title('Тест открытия выпадающего ответа при нажатии на четвертый вопрос')  # декораторы
-    def test_fourth_question(self, driver):
-        scooter_page = ScooterPage(driver)
-        scooter_page.go_to_site()
-        scooter_page.close_cookie()
-        scooter_page.click_one_question(Locators.FOUR_QUESTION)
-        element = scooter_page.element_text(Locators.FOUR_ANSWER)
-        assert element == Constants.ANSWER_FOUR
-
-    @allure.title('Тест открытия выпадающего ответа при нажатии на пятый вопрос')  # декораторы
-    def test_fifth_question(self, driver):
-        scooter_page = ScooterPage(driver)
-        scooter_page.go_to_site()
-        scooter_page.close_cookie()
-        scooter_page.click_one_question(Locators.FIVE_QUESTION)
-        element = scooter_page.element_text(Locators.FIVE_ANSWER)
-        assert element == Constants.ANSWER_FIVE
-
-    @allure.title('Тест открытия выпадающего ответа при нажатии на шестой вопрос')  # декораторы
-    def test_sixth_question(self, driver):
-        scooter_page = ScooterPage(driver)
-        scooter_page.go_to_site()
-        scooter_page.close_cookie()
-        scooter_page.click_one_question(Locators.SIX_QUESTION)
-        element = scooter_page.element_text(Locators.SIX_ANSWER)
-        assert element == Constants.ANSWER_SIX
-
-    @allure.title('Тест открытия выпадающего ответа при нажатии на седьмой вопрос')  # декораторы
-    def test_seventh_question(self, driver):
-        scooter_page = ScooterPage(driver)
-        scooter_page.go_to_site()
-        scooter_page.close_cookie()
-        scooter_page.click_one_question(Locators.SEVEN_QUESTION)
-        element = scooter_page.element_text(Locators.SEVEN_ANSWER)
-        assert element == Constants.ANSWER_SEVEN
-
-    @allure.title('Тест открытия выпадающего ответа при нажатии на восьмой вопрос')  # декораторы
-    def test_eighth_question(self, driver):
-        scooter_page = ScooterPage(driver)
-        scooter_page.go_to_site()
-        scooter_page.close_cookie()
-        scooter_page.click_one_question(Locators.EIGHT_QUESTION)
-        element = scooter_page.element_text(Locators.EIGHT_ANSWER)
-        assert element == Constants.ANSWER_EIGHT
+        scooter_page.click_one_question(data['question'])
+        element = scooter_page.element_text(data['answer'])
+        assert element == data['answer_text']
